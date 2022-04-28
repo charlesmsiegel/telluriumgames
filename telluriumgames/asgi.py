876629123297c07d12cd59e0451bd692c264bf46
Charles Siegel <charles.m.siegel@gmail.com>
2022-04-27 22:21:05 -0700

    update to asgi to include channels

--- telluriumgames/asgi.py
+++ telluriumgames/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for telluriumgames project.
+ASGI config for wodsite project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -9,8 +9,17 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'telluriumgames.settings')
+# import game.routing
 
-application = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "telluriumgames.settings")
+
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        # "websocket": AuthMiddlewareStack(URLRouter(game.routing.websocket_urlpatterns)),
+    }
+)
