e2fe9006ebc25364ba35cca41e296790025dc546
Charles Siegel <charles.m.siegel@gmail.com>
2022-04-27 22:28:58 -0700

    Wrote a todo for testing

--- characters/tests.py
+++ characters/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from characters.utils import weighted_choice
 
 # Create your tests here.
+class WeightedChoiceTest(TestCase):
+    # TODO: Write tests of weighted_choice
+    pass
