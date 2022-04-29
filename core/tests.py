e8d1ec3a95918ca6d9b834cc7644d3f8c2303b77
Charles Siegel <charles.m.siegel@gmail.com>
2022-04-29 15:03:41 -0700

    Basic homepage tests

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,98 @@
-from django.test import TestCase
+import time
 
+from django.contrib.auth.models import User
+from django.test import LiveServerTestCase, TestCase
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.support.ui import Select
+
+MAX_WAIT = 10
 # Create your tests here.
+# Create your tests here.
+class FunctionalTest(LiveServerTestCase):
+    """Base case for Functional Tests"""
+
+    def setUp(self) -> None:
+        options = webdriver.FirefoxOptions()
+        options.headless = True
+        self.browser = webdriver.Firefox(options=options)
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def wait_for_row_in_list_table(self, row_text):  # pragma: no cover
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id("id_character_table")
+                rows = table.find_elements_by_tag_name("tr")
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as exception:
+                if time.time() - start_time > MAX_WAIT:
+                    raise exception
+                time.sleep(0.5)
+
+    def clean_url(self, url):
+        return url.replace(self.live_server_url + "/", "")
+
+
+class TestHomeView(TestCase):
+    """Manages Tests for the HomeView and Template"""
+
+    def setUp(self) -> None:
+        User.objects.create_user("Test User", "test@user.com", "testpass")
+        User.objects.create_superuser("Test Admin", "admin@admin.com", "testadmin")
+
+    def test_home_status_code(self):
+        """Tests that the page exists"""
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_template(self):
+        """Tests that the correct template is loaded"""
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "core/index.html")
+
+    def test_content_null_user(self):
+        """Tests what users who are not logged in see on front page"""
+        response = self.client.get("/")
+        self.assertContains(response, "Tellurium Games")
+        self.assertContains(response, "navbar")
+        self.assertContains(response, "Log In")
+        self.assertContains(response, "Sign Up")
+
+    def test_content_logged_in(self):
+        """Tests what logged in users see"""
+        self.client.login(username="Test User", password="testpass")
+        response = self.client.get("/")
+        self.assertContains(response, "Tellurium Games")
+        self.assertContains(response, "navbar")
+        self.assertContains(response, "Log Out")
+        self.assertContains(response, "Account")
+
+    def test_content_staff(self):
+        """Tests what admins see"""
+        self.client.login(username="Test Admin", password="testadmin")
+        response = self.client.get("/")
+        self.assertContains(response, "Tellurium Games")
+        self.assertContains(response, "navbar")
+        self.assertContains(response, "Log Out")
+        self.assertContains(response, "Admin")
+        self.assertContains(response, "Account")
+
+    def test_includes_patreon(self):
+        """Tests site contains link to Patreon"""
+        self.fail("Test not implemented")
+
+    def test_includes_storytellers_vault(self):
+        """Tests site contains Stoyteller's Vault logo"""
+        self.fail("Test not implemented")
+
+    def test_includes_dark_pack(self):
+        """Tests site contains Dark Pack Logo"""
+        self.fail("Test not implemented")
+
+    def test_includes_storypath_nexus(self):
+        """Tests site contains Storypath Nexus logo"""
+        self.fail("Test not implemented")
