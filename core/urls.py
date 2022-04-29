d293166be9e8c5cbdc306718933e8d7b3ece7322
Charles Siegel <charles.m.siegel@gmail.com>
2022-04-29 13:47:21 -0700

    front page

---
+++ core/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+
+from core import views
+
+# Create your URLs here
+urlpatterns = [path("", views.HomeView.as_view(), name="home")]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import View
+
 
 # Create your views here.
+class HomeView(View):
+    """This View controls the main landing page for the site"""
+
+    def get(self, request):
+        context = {"user": request.user}
+        return render(request, "core/index.html", context=context)
