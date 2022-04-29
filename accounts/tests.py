fab919a22961a36b47a0a23c3c3bfd81f6ff75a7
Charles Siegel <charles.m.siegel@gmail.com>
2022-04-29 14:56:24 -0700

    Account test

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class TestSignUpView(TestCase):
+    """Class that Tests SignUpView"""
+
+    def test_correct_template(self):
+        self.client.get("/accounts/")
+        self.assertTemplateUsed("registration/login.html")
