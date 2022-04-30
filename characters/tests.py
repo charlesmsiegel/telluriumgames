f22c0b7616ae3965ffbb5dc338c09dd8eecfb7ab
Charles Siegel <charles.m.siegel@gmail.com>
2022-04-29 22:07:31 -0700

    added dots templatetag

--- characters/tests.py
+++ characters/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
+from characters.templatetags.dots import dots
+from collections import Counter
+
 # Create your tests here.
+class TestDots(TestCase):
+    def test_length(self):
+        output_5 = dots(4)
+        output_10 = dots(4, maximum=10)
+        output_10_2 = dots(6)
+        self.assertEqual(len(output_5), 5)
+        self.assertEqual(len(output_10), 10)
+        self.assertEqual(len(output_10_2), 10)
+        
+    def test_correct_ratio(self):
+        self.assertEqual(Counter(dots(3))["●"], 3)
+        self.assertEqual(Counter(dots(3))["○"], 2)
+        self.assertEqual(Counter(dots(3, maximum=10))["●"], 3)
+        self.assertEqual(Counter(dots(3, maximum=10))["○"], 7)
+        self.assertEqual(Counter(dots(6))["●"], 6)
+        self.assertEqual(Counter(dots(6))["○"], 4)
