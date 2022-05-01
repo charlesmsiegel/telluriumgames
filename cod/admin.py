b29d6c4c7ec557f08830b92573c55cb77423ada6
Charles Siegel <charles.m.siegel@gmail.com>
2022-05-01 14:14:13 -0700

    admin

--- cod/admin.py
+++ cod/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from cod.models import Mortal, Specialty, Merit
 
 # Register your models here.
+admin.register(Mortal)
+admin.register(Specialty)
+admin.register(Merit)
