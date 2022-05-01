ea1a1e5b4195861553d6a02061b1c23b46c13ce9
Charles Siegel <charles.m.siegel@gmail.com>
2022-05-01 14:32:38 -0700

    admin site fix

--- cod/admin.py
+++ cod/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from cod.models import Mortal, Specialty, Merit
 
 # Register your models here.
-admin.register(Mortal)
-admin.register(Specialty)
-admin.register(Merit)
+admin.site.register(Mortal)
+admin.site.register(Specialty)
+admin.site.register(Merit)
