85a3c5d1651588e4ca4d1a6510781b16763f30cc
Charles Siegel <charles.m.siegel@gmail.com>
2022-05-13 22:48:07 -0700

    attributes, skills, gettings, tweak to virtue/vice filter, and signatures of methods

--- cod/models/character/mortal.py
+++ cod/models/character/mortal.py
@@ -19,6 +19,45 @@ class Mortal(PolymorphicModel):
     virtue = models.CharField(max_length=100)
     vice = models.CharField(max_length=100)
 
+    intelligence = models.IntegerField(default=1)
+    wits = models.IntegerField(default=1)
+    resolve = models.IntegerField(default=1)
+
+    strength = models.IntegerField(default=1)
+    dexterity = models.IntegerField(default=1)
+    stamina = models.IntegerField(default=1)
+
+    presence = models.IntegerField(default=1)
+    manipulation = models.IntegerField(default=1)
+    composure = models.IntegerField(default=1)
+
+    academics = models.IntegerField(default=0)
+    computer = models.IntegerField(default=0)
+    crafts = models.IntegerField(default=0)
+    investigation = models.IntegerField(default=0)
+    medicine = models.IntegerField(default=0)
+    occult = models.IntegerField(default=0)
+    politics = models.IntegerField(default=0)
+    science = models.IntegerField(default=0)
+
+    athletics = models.IntegerField(default=0)
+    brawl = models.IntegerField(default=0)
+    drive = models.IntegerField(default=0)
+    firearms = models.IntegerField(default=0)
+    larceny = models.IntegerField(default=0)
+    stealth = models.IntegerField(default=0)
+    survival = models.IntegerField(default=0)
+    weaponry = models.IntegerField(default=0)
+
+    animal_ken = models.IntegerField(default=0)
+    empathy = models.IntegerField(default=0)
+    expression = models.IntegerField(default=0)
+    intimidation = models.IntegerField(default=0)
+    persuasion = models.IntegerField(default=0)
+    socialize = models.IntegerField(default=0)
+    streetwise = models.IntegerField(default=0)
+    subterfuge = models.IntegerField(default=0)
+
     merits = models.ManyToManyField("Merit", through="MeritRating")
 
     specialties = models.ManyToManyField("Specialty", blank=True)
@@ -39,10 +78,12 @@ class Mortal(PolymorphicModel):
         return self.virtue != ""
 
     def filter_virtues(self, virtue_list=None):
-        pass
+        virtues = [x for x in self.virtue.split(", ") if x]
+        return [x for x in virtue_list if x not in virtues]
 
     def random_virtue(self):
         virtues = ["Competitive", "Generous", "Just", "Loyal"]
+        virtues = self.filter_virtues(virtues)
         self.add_virtue(random.choice(virtues))
 
     def add_virtue(self, virtue):
@@ -58,10 +99,12 @@ class Mortal(PolymorphicModel):
         return self.vice != ""
 
     def filter_vices(self, vice_list=None):
-        pass
+        vices = [x for x in self.vice.split(", ") if x]
+        return [x for x in vice_list if x not in vices]
 
     def random_vice(self):
         vices = ["Competitive", "Generous", "Just", "Loyal"]
+        vices = self.filter_vices(vices)
         self.add_vice(random.choice(vices))
 
     def add_vice(self, vice):
@@ -76,14 +119,26 @@ class Mortal(PolymorphicModel):
     def add_attribute(self, attribute):
         add_dot(self, attribute, 5)
 
-    def get_physical_attributes(self):
-        pass
-
     def get_mental_attributes(self):
-        pass
-    
+        return {
+            "intelligence": self.intelligence,
+            "wits": self.wits,
+            "resolve": self.resolve,
+        }
+
+    def get_physical_attributes(self):
+        return {
+            "strength": self.strength,
+            "dexterity": self.dexterity,
+            "stamina": self.stamina,
+        }
+
     def get_social_attributes(self):
-        pass
+        return {
+            "presence": self.presence,
+            "manipulation": self.manipulation,
+            "composure": self.composure,
+        }
 
     def total_attributes(self):
         pass
@@ -100,14 +155,41 @@ class Mortal(PolymorphicModel):
     def random_attributes(self):
         pass
 
-    def get_physical_skills(self):
-        pass
-
     def get_mental_skills(self):
-        pass
-    
+        return {
+            "academics": self.academics,
+            "computer": self.computer,
+            "crafts": self.crafts,
+            "investigation": self.investigation,
+            "medicine": self.medicine,
+            "occult": self.occult,
+            "politics": self.politics,
+            "science": self.science,
+        }
+
+    def get_physical_skills(self):
+        return {
+            "athletics": self.athletics,
+            "brawl": self.brawl,
+            "drive": self.drive,
+            "firearms": self.firearms,
+            "larceny": self.larceny,
+            "stealth": self.stealth,
+            "survival": self.survival,
+            "weaponry": self.weaponry,
+        }
+
     def get_social_skills(self):
-        pass
+        return {
+            "animal_ken": self.animal_ken,
+            "empathy": self.empathy,
+            "expression": self.expression,
+            "intimidation": self.intimidation,
+            "persuasion": self.persuasion,
+            "socialize": self.socialize,
+            "streetwise": self.streetwise,
+            "subterfuge": self.subterfuge,
+        }
 
     def has_skills(self):
         pass
@@ -127,6 +209,9 @@ class Mortal(PolymorphicModel):
     def has_specialties(self):
         pass
 
+    def filter_specialties(self):
+        pass
+
     def add_specialty(self, specialty):
         pass
 
@@ -136,7 +221,7 @@ class Mortal(PolymorphicModel):
     def random_specialty(self):
         pass
 
-    def add_merit(self):
+    def add_merit(self, merit):
         pass
 
     def total_merits(self):
