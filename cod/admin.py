4e723bf0ceb073368dc1d34cf938ab41c0cd9766
Charles Siegel <charles.m.siegel@gmail.com>
2022-05-14 00:38:03 -0700

    set up classes for admin site

--- cod/admin.py
+++ cod/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from cod.models.character.mortal import Merit, Mortal, Specialty
+
+
 # Register your models here.
+@admin.register(Mortal)
+class MortalProfileAdmin(admin.ModelAdmin):
+    list_display = ("name", "player")
+
+
+@admin.register(Specialty)
+class SpecialtyProfileAdmin(admin.ModelAdmin):
+    list_display = ("name", "skill")
+
+
+@admin.register(Merit)
+class MeritProfileAdmin(admin.ModelAdmin):
+    list_display = ("name", "ratings")
