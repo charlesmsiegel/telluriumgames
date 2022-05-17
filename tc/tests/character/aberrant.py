7cbfc4c262b8b7a6347319dde1b4aefda8129224
Charles Siegel <charles.m.siegel@gmail.com>
2022-05-17 08:55:55 -0700

    Activated view tests and started working out what needs to be in other tests

--- tc/tests/character/aberrant.py
+++ tc/tests/character/aberrant.py
@@ -5,6 +5,10 @@ from tc.models.character.aberrant import Aberrant
 
 # Create your tests here.
 # class TestAberrant(TestCase):
+#     def setUp(self):
+#         self.player = User.objects.create(username="Test User")
+#         self.character = Aberrant.objects.create(name="", player=self.player.tc_profile)
+
 #     def test_add_megaedge(self):
 #         self.fail()
 
@@ -65,7 +69,27 @@ from tc.models.character.aberrant import Aberrant
 #     def test_assign_advantages(self):
 #         self.fail()
 
+#     def test_has_template(self):
+#         self.fail("Quantum of 1")
+#         self.fail("One dot in favored approach")
+#         self.fail("Either 1 dot of Fame or 1 dot of Alternate Identity Edge")
+#         self.fail("150 XP")
+#         self.fail("Check spend_xp?")
+
 #     def test_xp_cost(self):
+#         self.fail("Human costs too")
+#         self.fail("Mega Attribute Cost")
+#         self.fail("Mega Attribute Cost With Transcendence")
+#         self.fail("Mega Edge Cost")
+#         self.fail("Mega Edge Cost With Transcendence")
+#         self.fail("Power Tag Cost")
+#         self.fail("Quantum <= 5 Cost")
+#         self.fail("Quantum > 5 Cost")
+#         self.fail("Quantum Power Cost")
+#         self.fail("Quantum Power Cost With Transcendence")
+#         self.fail("Remove Tag Cost")
+
+#     def test_spend_xp(self):
 #         self.fail()
 
 
@@ -83,18 +107,18 @@ from tc.models.character.aberrant import Aberrant
 #         self.fail()
 
 
-# class TestAberrantDetailView(TestCase):
-#     def setUp(self) -> None:
-#         User.objects.create_user("Test User", "test@user.com", "testpass")
-#         self.character = Aberrant.objects.create(
-#             name="Test Character",
-#             player=User.objects.get(username="Test User").tc_profile,
-#         )
+class TestAberrantDetailView(TestCase):
+    def setUp(self) -> None:
+        User.objects.create_user("Test User", "test@user.com", "testpass")
+        self.character = Aberrant.objects.create(
+            name="Test Character",
+            player=User.objects.get(username="Test User").tc_profile,
+        )
 
-#     def test_mortal_detail_view_status_code(self):
-#         response = self.client.get(f"/tc/characters/{self.character.id}/")
-#         self.assertEqual(response.status_code, 200)
+    def test_mortal_detail_view_status_code(self):
+        response = self.client.get(f"/tc/characters/{self.character.id}/")
+        self.assertEqual(response.status_code, 200)
 
-#     def test_mortal_detail_view_template(self):
-#         response = self.client.get(f"/tc/characters/{self.character.id}/")
-#         self.assertTemplateUsed(response, "tc/characters/aberrant/detail.html")
+    def test_mortal_detail_view_template(self):
+        response = self.client.get(f"/tc/characters/{self.character.id}/")
+        self.assertTemplateUsed(response, "tc/characters/aberrant/detail.html")
