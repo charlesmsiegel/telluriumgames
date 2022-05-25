764f262daf953a6dcad758ab5383f8c88ba4f597
Charles Siegel <charles.m.siegel@gmail.com>
2022-05-25 08:45:20 -0700

    implemented several methods on Human related to attributes 65F

--- wod/models/characters/human.py
+++ wod/models/characters/human.py
@@ -2,6 +2,7 @@ from django.db import models
 from polymorphic.models import PolymorphicModel
 
 from accounts.models import WoDProfile
+from core.utils import add_dot, weighted_choice
 
 
 # Create your models here.
@@ -36,38 +37,64 @@ class Character(PolymorphicModel):
 class Human(Character):
     type = "human"
 
+    strength = models.IntegerField(default=1)
+    dexterity = models.IntegerField(default=1)
+    stamina = models.IntegerField(default=1)
+    perception = models.IntegerField(default=1)
+    intelligence = models.IntegerField(default=1)
+    wits = models.IntegerField(default=1)
+    charisma = models.IntegerField(default=1)
+    manipulation = models.IntegerField(default=1)
+    appearance = models.IntegerField(default=1)
+
     def has_archetypes(self):
         pass
 
     def set_archetypes(self, nature, demeanor):
         pass
 
-    def add_attribute(self, attribute):
-        pass
+    def add_attribute(self, attribute, maximum=5):
+        return add_dot(self, attribute, maximum)
 
     def get_attributes(self):
-        pass
+        tmp = {}
+        tmp.update(self.get_physical_attributes())
+        tmp.update(self.get_mental_attributes())
+        tmp.update(self.get_social_attributes())
+        return tmp
 
     def get_physical_attributes(self):
-        pass
+        return {
+            "strength": self.strength,
+            "dexterity": self.dexterity,
+            "stamina": self.stamina
+        }
 
     def get_social_attributes(self):
-        pass
+        return {
+            "charisma": self.charisma,
+            "manipulation": self.manipulation,
+            "appearance": self.appearance
+        }
 
     def get_mental_attributes(self):
-        pass
+        return {
+            "perception": self.perception,
+            "intelligence": self.intelligence,
+            "wits": self.wits
+        }
 
     def total_physical_attributes(self):
-        return 0
+        return sum(self.get_physical_attributes().values())
 
     def total_social_attributes(self):
-        return 0
+        return sum(self.get_social_attributes().values())
 
     def total_mental_attributes(self):
-        return 0
+        return sum(self.get_mental_attributes().values())
 
     def filter_attributes(self, minimum=0, maximum=5):
-        return []
+        return {k: v for k, v in self.get_attributes().items() if minimum <= v <= maximum}
 
     def total_talents(self):
         return 0
