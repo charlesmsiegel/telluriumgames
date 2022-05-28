adffaed7d562c26057db067f0bf4c09fbeaa0fa5
Charles Siegel <charles.m.siegel@gmail.com>
2022-05-28 08:31:51 -0700

    Implemented remaining human tests in TC

--- tc/tests/characters/human.py
+++ tc/tests/characters/human.py
@@ -17,10 +17,22 @@ from tc.models.characters.talent import Talent
 # Create your tests here.
 class TestPath(TestCase):
     def test_has_connection(self):
-        self.fail()
+        p = Path.objects.create(name="Path")
+        c = PathConnection.objects.create(name="Connection")
+        h = Human.objects.create(name="Test")
+        h.add_path(p)
+        self.assertFalse(h.has_connection(p))
+        h.set_connection(p, c)
+        self.assertTrue(h.has_connection(p))
 
-    def test_set_connect(self):
-        self.fail()
+    def test_set_connectiont(self):
+        p = Path.objects.create(name="Path")
+        c = PathConnection.objects.create(name="Connection")
+        h = Human.objects.create(name="Test")
+        h.add_path(p)
+        self.assertFalse(h.has_connection(p))
+        self.assertTrue(h.set_connection(p, c))
+        self.assertTrue(h.has_connection(p))
 
 
 class TestHuman(TestCase):
@@ -625,8 +637,23 @@ class TestHuman(TestCase):
         self.assertEqual(self.character.approach, "FIN")
 
     def test_spent_xp(self):
-        # This will test keeping a record of XP expenditures
-        self.fail()
+        self.character.xp = 100
+        self.assertEqual(self.character.spent_xp, "")
+        self.assertEqual(self.character.might, 1)
+        self.character.spend_xp("might")
+        self.assertEqual(self.character.might, 2)
+        self.assertEqual(self.character.spent_xp, "Might 2 (10 XP)")
+        self.character.spend_xp("might")
+        self.assertEqual(self.character.strength, 3)
+        self.assertEqual(
+            self.character.spent_xp, "Might 2 (10 XP), Might 3 (10 XP)"
+        )
+        self.character.spend_xp("science")
+        self.assertEqual(self.character.science, 1)
+        self.assertEqual(
+            self.character.spent_xp,
+            self.character.spent_xp, "Might 2 (10 XP), Might 3 (10 XP), Science 1 (5 XP)"
+        )
 
     def test_get_absolute_url(self):
         self.assertEqual(
