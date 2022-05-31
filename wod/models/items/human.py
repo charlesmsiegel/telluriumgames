9eb1953b5a9703ad3eb5569737dfb93da491a2a4
Charles Siegel <charles.m.siegel@gmail.com>
2022-05-30 22:48:40 -0700

    added get_absolute_url

--- wod/models/items/human.py
+++ wod/models/items/human.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from polymorphic.models import PolymorphicModel
 
 
@@ -10,3 +11,6 @@ class Item(PolymorphicModel):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("wod:item", args=[str(self.id)])
