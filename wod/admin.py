a2da9f5d891f3210e660b9b5e8ada36e2739ac9e
Charles Siegel <charles.m.siegel@gmail.com>
2022-06-01 08:56:09 -0700

    set up wod Admin site

--- wod/admin.py
+++ wod/admin.py
@@ -16,84 +16,84 @@ from wod.models.locations.human import City, Location
 from wod.models.locations.mage import Node, NodeMeritFlaw
 
 # Register your models here.
-# @admin.register(Character)
-# class CharacterAdmin(admin.ModelAdmin):
-#     list_display = ("name", "player", "status")
+@admin.register(Character)
+class CharacterAdmin(admin.ModelAdmin):
+    list_display = ("name", "player")
 
 
-# @admin.register(Human)
-# class HumanCharacterAdmin(admin.ModelAdmin):
-#     list_display = ("name", "player", "status")
+@admin.register(Human)
+class HumanCharacterAdmin(admin.ModelAdmin):
+    list_display = ("name", "player")
 
 
-# @admin.register(Mage)
-# class MageAdmin(admin.ModelAdmin):
-#     list_display = ("name", "player", "status", "arete", "faction")
+@admin.register(Mage)
+class MageAdmin(admin.ModelAdmin):
+    list_display = ("name", "player", "arete", "faction")
 
 
-# @admin.register(MageFaction)
-# class MageFactionAdmin(admin.ModelAdmin):
-#     list_display = ("name", "parent")
+@admin.register(MageFaction)
+class MageFactionAdmin(admin.ModelAdmin):
+    list_display = ("name", "parent")
 
 
-# @admin.register(Paradigm)
-# class ParadigmAdmin(admin.ModelAdmin):
-#     list_display = ("name",)
+@admin.register(Paradigm)
+class ParadigmAdmin(admin.ModelAdmin):
+    list_display = ("name",)
 
 
-# @admin.register(Practice)
-# class PracticeAdmin(admin.ModelAdmin):
-#     list_display = ("name",)
+@admin.register(Practice)
+class PracticeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
 
 
-# @admin.register(Instrument)
-# class InstrumentAdmin(admin.ModelAdmin):
-#     list_display = ("name",)
+@admin.register(Instrument)
+class InstrumentAdmin(admin.ModelAdmin):
+    list_display = ("name",)
 
 
-# @admin.register(Resonance)
-# class ResonanceAdmin(admin.ModelAdmin):
-#     list_display = (
-#         "name",
-#         "correspondence",
-#         "entropy",
-#         "forces",
-#         "life",
-#         "matter",
-#         "mind",
-#         "prime",
-#         "spirit",
-#         "time",
-#     )
+@admin.register(Resonance)
+class ResonanceAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "correspondence",
+        "entropy",
+        "forces",
+        "life",
+        "matter",
+        "mind",
+        "prime",
+        "spirit",
+        "time",
+    )
 
-# @admin.register(Rote)
-# class RoteAdmin(admin.ModelAdmin):
-#     list_display = (
-#         "name",
-#         "correspondence",
-#         "entropy",
-#         "forces",
-#         "life",
-#         "matter",
-#         "mind",
-#         "prime",
-#         "spirit",
-#         "time",
-#     )
+@admin.register(Rote)
+class RoteAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "correspondence",
+        "entropy",
+        "forces",
+        "life",
+        "matter",
+        "mind",
+        "prime",
+        "spirit",
+        "time",
+    )
 
-# admin.site.register(Location)
-# admin.site.register(City)
-# admin.site.register(Node)
-
-
-# @admin.register(Wonder)
-# class WonderAdmin(admin.ModelAdmin):
-#     list_display = ("name", "rank", "background_cost", "quintessence_max")
+admin.site.register(Location)
+admin.site.register(City)
+admin.site.register(Node)
 
 
-# @admin.register(Grimoire)
-# class GrimoireAdmin(admin.ModelAdmin):
-#     list_display = ("name", "rank", "primer", "medium")
+@admin.register(Wonder)
+class WonderAdmin(admin.ModelAdmin):
+    list_display = ("name", "rank", "background_cost", "quintessence_max")
 
 
-# admin.site.register(Library)
+@admin.register(Grimoire)
+class GrimoireAdmin(admin.ModelAdmin):
+    list_display = ("name", "rank", "is_primer", "medium")
+
+
+admin.site.register(Library)
