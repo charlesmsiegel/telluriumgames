f59f5a01dc2f90090e55c8dbecc4a437dfdc7d25
Charles Siegel <charles.m.siegel@gmail.com>
2022-06-01 15:51:42 -0700

    Fixed output issue when objects not fully initialized

--- core/templatetags/dots.py
+++ core/templatetags/dots.py
@@ -5,6 +5,8 @@ register = template.Library()
 
 @register.filter(name="dots")
 def dots(value, maximum=5):
+    if not isinstance(value, int):
+        return value
     if value > maximum:
         maximum = 10
     return "●" * value + "○" * (maximum - value)
