b6fe7438a8816859f6062b5acb88e358e4e8a29f
Charles Siegel <charles.m.siegel@gmail.com>
2022-06-13 20:28:44 -0700

    tweaked probability for node resonance when attached to mage

--- wod/models/locations/mage.py
+++ wod/models/locations/mage.py
@@ -164,10 +164,10 @@ class Node(Location):
             if favored_list is not None:
                 choices = {i: 1 for i in range(1, Resonance.objects.last().id + 1)}
                 for resonance in favored_list:
-                    choices[resonance.id] += Resonance.objects.last().id // 2
+                    choices[resonance.id] += 100
             else:
                 choices = {i: 1 for i in range(1, Resonance.objects.last().id + 1)}
-            index = weighted_choice(choices, ceiling=1000000)
+            index = weighted_choice(choices, floor=1, ceiling=1000000)
             if Resonance.objects.filter(pk=index).exists():
                 choice = Resonance.objects.get(pk=index)
                 if self.check_resonance(choice, sphere=sphere):
