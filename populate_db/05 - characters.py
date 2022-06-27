824b55c4701c9c0ee6f212c62c493377285579de
Charles Siegel <charles.m.siegel@gmail.com>
2022-06-27 08:14:04 -0700

    uncommented creations

--- populate_db/05 - characters.py
+++ populate_db/05 - characters.py
@@ -2,51 +2,51 @@ from time import time
 
 from django.contrib.auth.models import User
 
-player = User.objects.get(username="Test")
+player, _ = User.objects.get_or_create(username="Test")
 
-# from wod.models.characters.mage import Mage
-# from wod.models.characters.werewolf import Werewolf
-# from wod.models.items.mage import Grimoire
-# from wod.models.locations.mage import Node
+from wod.models.characters.mage import Mage
+from wod.models.characters.werewolf import Werewolf
+from wod.models.items.mage import Grimoire
+from wod.models.locations.mage import Node
 
-# mage_start = time()
-# for i in range(10):
-#     mage = Werewolf.objects.create(name=f"Werewolf {Werewolf.objects.count()}", player=player.wod_profile)
-#     mage.random()
-#     mage.save()
-# print("Average Random Werewolf Time:", (time() - mage_start) / 10, "original", "1.46")
+mage_start = time()
+for i in range(10):
+    mage = Werewolf.objects.create(name=f"Werewolf {Werewolf.objects.count()}", player=player.wod_profile)
+    mage.random()
+    mage.save()
+print("Average Random Werewolf Time:", (time() - mage_start) / 10, "original", "1.46")
 
-# mage_start = time()
-# for i in range(10):
-#     mage = Mage.objects.create(name=f"Mage {Mage.objects.count()}", player=player.wod_profile)
-#     mage.random()
-#     mage.save()
-# print("Average Random Mage Time:", (time() - mage_start) / 10, "original", "1.21")
-
-
-# node_start = time()
-# for i in range(10):
-#     node = Node.objects.create(name=f"Node {Node.objects.count()}")
-#     node.random(rank=(i % 5) + 1)
-#     node.save()
-# print("Average Random Node Time:", (time() - node_start) / 10, "original", "0.09")
+mage_start = time()
+for i in range(10):
+    mage = Mage.objects.create(name=f"Mage {Mage.objects.count()}", player=player.wod_profile)
+    mage.random()
+    mage.save()
+print("Average Random Mage Time:", (time() - mage_start) / 10, "original", "1.21")
 
 
-# grimoire_start = time()
-# for i in range(10):
-#     grimoire = Grimoire.objects.create(name=f"Grimoire {Grimoire.objects.count()}")
-#     grimoire.random(rank=(i % 5) + 1)
-#     grimoire.save()
-# print("Average Random Grimoire Time:", (time() - grimoire_start) / 10, "original", "0.07")
+node_start = time()
+for i in range(10):
+    node = Node.objects.create(name=f"Node {Node.objects.count()}")
+    node.random(rank=(i % 5) + 1)
+    node.save()
+print("Average Random Node Time:", (time() - node_start) / 10, "original", "0.09")
 
-# from cod.models.characters.mortal import Mortal
 
-# mortal_start = time()
-# for i in range(10):
-#     mortal = Mortal.objects.create(name=f"Mortal {Mortal.objects.count()}", player=player.cod_profile)
-#     mortal.random()
-#     mortal.save()
-# print("Average CoD Mortal Time:", (time() - mortal_start) / 10, "original", "0.85")
+grimoire_start = time()
+for i in range(10):
+    grimoire = Grimoire.objects.create(name=f"Grimoire {Grimoire.objects.count()}")
+    grimoire.random(rank=(i % 5) + 1)
+    grimoire.save()
+print("Average Random Grimoire Time:", (time() - grimoire_start) / 10, "original", "0.07")
+
+from cod.models.characters.mortal import Mortal
+
+mortal_start = time()
+for i in range(10):
+    mortal = Mortal.objects.create(name=f"Mortal {Mortal.objects.count()}", player=player.cod_profile)
+    mortal.random()
+    mortal.save()
+print("Average CoD Mortal Time:", (time() - mortal_start) / 10, "original", "0.85")
 
 from tc.models.characters.aberrant import Aberrant
 from tc.models.characters.human import Human
