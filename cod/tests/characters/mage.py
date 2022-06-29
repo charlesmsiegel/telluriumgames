4455e29c719d7fc896c58912a8266118f2bcd44a
Charles Siegel <charles.m.siegel@gmail.com>
2022-06-28 22:05:39 -0700

    created skeleton of tests

--- cod/tests/characters/mage.py
+++ cod/tests/characters/mage.py
@@ -3,12 +3,97 @@ from django.test import TestCase
 
 # Create your tests here.
 class TestMage(TestCase):
-    pass
+    def test_has_path(self):
+        self.fail()
+
+    def test_set_path(self):
+        self.fail()
+
+    def test_has_order(self):
+        self.fail()
+
+    def test_set_order(self):
+        self.fail()
+
+    def test_has_legacy(self):
+        self.fail()
+
+    def test_set_legacy(self):
+        self.fail()
+
+    def test_has_rote_skills(self):
+        self.fail()
+
+    def test_set_rote_skills(self):
+        self.fail()
+
+    def test_get_arcana(self):
+        self.fail()
+
+    def test_has_arcana(self):
+        self.fail()
+
+    def test_add_arcana(self):
+        self.fail()
+
+    def test_filter_arcana(self):
+        self.fail()
+
+    def test_xp_cost(self):
+        self.fail()
+
+    def test_add_gnosis(self):
+        self.fail()
+
+    def test_has_gnosis(self):
+        self.fail()
+
+    def test_set_gnosis(self):
+        self.fail()
+
+    def test_has_mana(self):
+        self.fail()
+
+    def test_set_mana(self):
+        self.fail()
+
+    def test_has_rotes(self):
+        self.fail()
+
+    def test_set_rotes(self):
+        self.fail()
+
+    def test_add_rote(self):
+        self.fail()
+
+    def test_has_numbus(self):
+        self.fail()
+
+    def test_set_nimbus(self):
+        self.fail()
 
 
 class TestRandomMage(TestCase):
-    pass
+    def test_random_path(self):
+        self.fail()
 
+    def test_random_order(self):
+        self.fail()
 
-class TestMageDetailView(TestCase):
-    pass
+    def test_random_legacy(self):
+        self.fail()
+
+    def test_random_arcana(self):
+        self.fail()
+
+    def test_random_rote(self):
+        self.fail()
+
+    def test_random_nimbus(self):
+        self.fail()
+
+    def test_random_spend_xp(self):
+        self.fail()
+
+    def test_random(self):
+        self.fail()
