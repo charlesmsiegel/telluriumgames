dea9eb92dc49449f3e9825984108436264daec77
Charles Siegel <charles.m.siegel@gmail.com>
2022-07-04 08:38:06 -0700

    views, urls, and blank template

--- exalted/urls.py
+++ exalted/urls.py
@@ -3,4 +3,12 @@ from django.urls import path
 from exalted import views
 
 # Create your URLs here
-urlpatterns = []
+urlpatterns = [
+    path("characters/", views.IndexView.as_view(), name="characters_index"),
+    path(
+        "characters/random/",
+        views.RandomCharacterView.as_view(),
+        name="random_character",
+    ),
+    path("characters/<pk>/", views.CharacterDetailView.as_view(), name="character"),
+]
