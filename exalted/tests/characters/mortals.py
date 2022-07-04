58cf1eeb9ff8f4ca93022ed560c356f97236c08a
Charles Siegel <charles.m.siegel@gmail.com>
2022-07-04 08:37:54 -0700

    small tweak

--- exalted/tests/characters/mortals.py
+++ exalted/tests/characters/mortals.py
@@ -666,5 +666,5 @@ class TestGenericCharacterDetailViews(TestCase):
         )
 
     def test_character_detail_view_templates(self):
-        response = self.client.get(f"/exalted/characters/{self.character.id}/")
+        response = self.client.get(f"/exalted/characters/{self.mortal.id}/")
         self.assertTemplateUsed(response, "exalted/characters/mortal/detail.html")
