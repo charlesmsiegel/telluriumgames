4ba7b55c1f0e53101d00962a560992b970dc8480
Charles Siegel <charles.m.siegel@gmail.com>
2022-07-03 19:10:50 -0700

    outlining tests for mortals in exalted

---
+++ exalted/tests/characters/mortals.py
@@ -0,0 +1,113 @@
+from django.test import TestCase
+
+
+# Create your tests here.
+class TestMortal(TestCase):
+    def test_has_concept(self):
+        self.fail()
+
+    def test_set_concept(self):
+        self.fail()
+
+    def test_add_attribute(self):
+        self.fail()
+
+    def test_get_attributes(self):
+        self.fail()
+
+    def test_has_attributes(self):
+        self.fail()
+
+    def test_filter_attributes(self):
+        self.fail()
+
+    def test_add_ability(self):
+        self.fail()
+
+    def test_get_abilities(self):
+        self.fail()
+
+    def test_has_abilities(self):
+        self.fail()
+
+    def test_filter_abilities(self):
+        self.fail()
+
+    def test_add_specialty(self):
+        self.fail()
+
+    def test_has_specialties(self):
+        self.fail()
+
+    def test_filter_specialties(self):
+        self.fail()
+
+    def test_add_merit(self):
+        self.fail()
+
+    def test_filter_merits(self):
+        self.fail()
+
+    def test_has_intimacies(self):
+        self.fail()
+
+    def test_add_intimacy(self):
+        self.fail()
+
+    def test_bonus_point_costs(self):
+        self.fail()
+
+    def test_spend_bonus_points(self):
+        self.fail()
+
+    def test_xp_costs(self):
+        self.fail()
+
+    def test_spend_xp(self):
+        self.fail()
+
+
+class TestRandomMortal(TestCase):
+    def test_random_concept(self):
+        self.fail()
+
+    def test_random_attribute(self):
+        self.fail()
+
+    def test_random_attributes(self):
+        self.fail()
+
+    def test_random_ability(self):
+        self.fail()
+
+    def test_random_abilities(self):
+        self.fail()
+
+    def test_random_specialty(self):
+        self.fail()
+
+    def test_random_specialties(self):
+        self.fail()
+
+    def test_random_merit(self):
+        self.fail()
+
+    def test_random_intimacy(self):
+        self.fail()
+
+    def test_random_spend_bonus_points(self):
+        self.fail()
+
+    def test_random_spend_xp(self):
+        self.fail()
+
+    def test_random(self):
+        self.fail()
+
+
+class TestMortalDetailView(TestCase):
+    def test_mortal_detail_view_status_code(self):
+        self.fail()
+
+    def test_mortal_detail_view_template(self):
+        self.fail()
