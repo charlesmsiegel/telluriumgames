6c40add970f1e8a4cf5da65cf0969e6f932e7e61
Charles Siegel <charles.m.siegel@gmail.com>
2022-08-13 13:14:57 -0700

    updated urls and views and templates

--- wod/urls/items/human.py
+++ wod/urls/items/human.py
@@ -11,4 +11,39 @@ urls = [
         views.items.human.ItemUpdateView.as_view(),
         name="update_item",
     ),
+
+
+
+    path(
+        "weapon/create/", views.items.human.WeaponCreateView.as_view(), name="create_weapon"
+    ),
+    path(
+        "weapon/update/<pk>/",
+        views.items.human.WeaponUpdateView.as_view(),
+        name="update_weapon",
+    ),
+    path(
+        "meleeweapon/create/", views.items.human.MeleeWeaponCreateView.as_view(), name="create_meleeweapon"
+    ),
+    path(
+        "meleeweapon/update/<pk>/",
+        views.items.human.MeleeWeaponUpdateView.as_view(),
+        name="update_meleeweapon",
+    ),
+    path(
+        "rangedweapon/create/", views.items.human.RangedWeaponCreateView.as_view(), name="create_rangedweapon"
+    ),
+    path(
+        "rangedweapon/update/<pk>/",
+        views.items.human.RangedWeaponUpdateView.as_view(),
+        name="update_rangedweapon",
+    ),
+    path(
+        "thrownweapon/create/", views.items.human.ThrownWeaponCreateView.as_view(), name="create_thrownweapon"
+    ),
+    path(
+        "thrownweapon/update/<pk>/",
+        views.items.human.ThrownWeaponUpdateView.as_view(),
+        name="update_thrownweapon",
+    ),
 ]
--- wod/views/items/human.py
+++ wod/views/items/human.py
@@ -1,6 +1,6 @@
 from django.views.generic import CreateView, DetailView, UpdateView
 
-from wod.models.items.human import Item
+from wod.models.items.human import Item, Weapon, MeleeWeapon, RangedWeapon, ThrownWeapon
 
 
 class ItemDetailView(DetailView):
@@ -18,3 +18,69 @@ class ItemUpdateView(UpdateView):
     model = Item
     fields = "__all__"
     template_name = "wod/items/human/item/create.html"
+
+
+
+class WeaponDetailView(DetailView):
+    model = Weapon
+    template_name = "wod/items/human/weapon/detail.html"
+
+
+class WeaponCreateView(CreateView):
+    model = Weapon
+    fields = "__all__"
+    template_name = "wod/items/human/weapon/create.html"
+
+
+class WeaponUpdateView(UpdateView):
+    model = Weapon
+    fields = "__all__"
+    template_name = "wod/items/human/weapon/create.html"
+
+class MeleeWeaponDetailView(DetailView):
+    model = MeleeWeapon
+    template_name = "wod/items/human/meleeweapon/detail.html"
+
+
+class MeleeWeaponCreateView(CreateView):
+    model = MeleeWeapon
+    fields = "__all__"
+    template_name = "wod/items/human/meleeweapon/create.html"
+
+
+class MeleeWeaponUpdateView(UpdateView):
+    model = MeleeWeapon
+    fields = "__all__"
+    template_name = "wod/items/human/meleeweapon/create.html"
+
+class RangedWeaponDetailView(DetailView):
+    model = RangedWeapon
+    template_name = "wod/items/human/rangedweapon/detail.html"
+
+
+class RangedWeaponCreateView(CreateView):
+    model = RangedWeapon
+    fields = "__all__"
+    template_name = "wod/items/human/rangedweapon/create.html"
+
+
+class RangedWeaponUpdateView(UpdateView):
+    model = RangedWeapon
+    fields = "__all__"
+    template_name = "wod/items/human/rangedweapon/create.html"
+
+class ThrownWeaponDetailView(DetailView):
+    model = ThrownWeapon
+    template_name = "wod/items/human/thrownweapon/detail.html"
+
+
+class ThrownWeaponCreateView(CreateView):
+    model = ThrownWeapon
+    fields = "__all__"
+    template_name = "wod/items/human/thrownweapon/create.html"
+
+
+class ThrownWeaponUpdateView(UpdateView):
+    model = ThrownWeapon
+    fields = "__all__"
+    template_name = "wod/items/human/thrownweapon/create.html"
