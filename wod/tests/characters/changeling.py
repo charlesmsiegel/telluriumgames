303666a43b8e168d943938c1fb856f60166bb4aa
Charles Siegel <charles.m.siegel@gmail.com>
2022-09-02 23:27:19 -0700

    Initial work on CtD work

--- wod/tests/characters/changeling.py
+++ wod/tests/characters/changeling.py
@@ -1,12 +1,57 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+
+from wod.models.characters.changeling import Changeling
 
 
 # Create your tests here.
-class TestChangeling(TestCase):
+class TestKith(TestCase):
     pass
 
 
+class TestHouse(TestCase):
+    pass
+
+
+class TestCtDHuman(TestCase):
+    pass
+
+
+class TestChangeling(TestCase):
+    def setUp(self) -> None:
+        self.player = User.objects.create_user(username="User1", password="12345")
+        self.character = Changeling.objects.create(owner=self.player, name="")
+
+    def test_set_seeming(self):
+        self.assertFalse(self.character.has_seeming())
+        glamour = self.character.glamour
+        willpower = self.character.willpower
+        self.assertTrue(self.character.set_seeming("childer"))
+        self.assertEqual(self.character.glamour, glamour + 1)
+        self.assertTrue(self.character.has_seeming())
+        self.assertTrue(self.character.set_seeming("wilder"))
+        self.assertEqual(self.character.glamour + self.character.willpower, glamour + willpower + 1)
+        self.assertTrue(self.character.set_seeming("grump")) 
+        self.assertEqual(self.character.willpower, willpower + 1)
+
+    def test_has_seeming(self):
+        self.assertFalse(self.character.has_seeming())
+        self.character.set_seeming("childer")
+        self.assertTrue(self.character.has_seeming())
+
+
 class TestRandomChangeling(TestCase):
+    def setUp(self) -> None:
+        self.player = User.objects.create_user(username="User1", password="12345")
+        self.character = Changeling.objects.create(owner=self.player, name="")
+
+    def test_random_seeming(self):
+        self.assertFalse(self.character.has_seeming())
+        self.assertTrue(self.character.random_seeming())
+        self.assertTrue(self.character.has_seeming())
+
+
+class TestMotley(TestCase):
     pass
 
 
--- wod/tests/locations/changeling.py
+++ wod/tests/locations/changeling.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+class TestGlade(TestCase):
+    pass
+
+
+class TestFreehold(TestCase):
+    pass
+
+
+class TestNobleLand(TestCase):
+    pass
