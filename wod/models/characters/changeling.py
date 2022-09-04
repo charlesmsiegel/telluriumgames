afb809db0032ef03e44352f82182adbc6eff6e85
Charles Siegel <charles.m.siegel@gmail.com>
2022-09-03 23:31:13 -0700

    All classes exist

--- wod/models/characters/changeling.py
+++ wod/models/characters/changeling.py
@@ -1,4 +1,21 @@
 from django.db import models
+from wod.models.characters.human import Human, Group
+from core.models import Model
+
+class Kith(Model):
+    type = "kith"
+    
+class House(Model):
+    type = "house"
+
+class CtDLegacy(Model):
+    type = "legacy"
+
+class CtDHuman(Human):
+    type = "ctd_human"
+
+class Changeling(CtDHuman):
+    type = "changeling"
 
 # Create your models here.
 # # Talents
@@ -28,3 +45,6 @@ from django.db import models
 # Retinue
 # Title
 # Treasure
+
+class Motley(Group):
+    type = "motley"
