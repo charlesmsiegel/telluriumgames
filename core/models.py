3b8f89bb637b2b624cb4875df039c5f0d1d8a627
Charles Siegel <charles.m.siegel@gmail.com>
2022-09-08 07:13:07 -0700

    Freeform Status Fixes #284

--- core/models.py
+++ core/models.py
@@ -99,8 +99,8 @@ class Model(PolymorphicModel):
     name = models.CharField(max_length=100)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
 
-    status_keys = ["Un", "Sub", "App", "Ret", "Dec", "Ran"]
-    statuses = ["Unfinished", "Submitted", "Approved", "Retired", "Deceased", "Random"]
+    status_keys = ["Un", "Sub", "App", "Ret", "Dec", "Ran", "Fre"]
+    statuses = ["Unfinished", "Submitted", "Approved", "Retired", "Deceased", "Random", "Freeform"]
     status = models.CharField(
         max_length=3, choices=zip(status_keys, statuses), default="Un"
     )
