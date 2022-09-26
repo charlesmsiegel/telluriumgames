9e03553e1eb75f6fbd65e84890a698d58032243b
Charles Siegel <charles.m.siegel@gmail.com>
2022-09-26 00:40:52 -0700

    Basics of urls, views, and templates

--- game/urls.py
+++ game/urls.py
@@ -5,4 +5,6 @@ from game import views
 # Create your URLs here
 urlpatterns = [
     path("chronicle/<pk>", views.ChronicleDetailView.as_view(), name="chronicle"),
+    path("story/<pk>", views.StoryDetailView.as_view(), name="story"),
+    path("scene/<pk>", views.SceneDetailView.as_view(), name="scene"),
 ]
