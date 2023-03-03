a691cc47859124ecee5de58b22888c4940e55954
Charles Siegel <charles.m.siegel@gmail.com>
2023-03-03 01:13:01 -0800

    tests for cod ephemera

--- cod/tests/characters/ephemera.py
+++ cod/tests/characters/ephemera.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from cod.models.characters.ephemera import Ephemera
+from cod.models.characters.ephemera import Ephemera, Numina
 
 
 # Create your tests here.
@@ -46,39 +46,98 @@ class TestEphemera(TestCase):
         self.assertEqual(self.ephemera.maximum_essence, 50)
 
     def test_get_attributes(self):
-        self.fail()
+        self.ephemera.power = 2
+        self.ephemera.finesse = 3
+        self.ephemera.resistance = 4
+        expected_attributes = {"power": 2, "finesse": 3, "resistance": 4}
+        self.assertEqual(self.ephemera.get_attributes(), expected_attributes)
 
     def test_total_attributes(self):
-        self.fail()
+        self.ephemera.power = 2
+        self.ephemera.finesse = 3
+        self.ephemera.resistance = 4
+        self.assertEqual(self.ephemera.total_attributes(), 9)
 
     def test_other_traits(self):
-        self.fail()
+        self.ephemera.rank = 1
+        self.ephemera.power = 2
+        self.ephemera.finesse = 3
+        self.ephemera.resistance = 4
+        self.ephemera.other_traits()
+        self.assertEqual(self.ephemera.corpus, 9)
+        self.assertEqual(self.ephemera.willpower, 7)
+        self.assertEqual(self.ephemera.initiative, 7)
+        self.assertEqual(self.ephemera.defense, 2)
+        self.assertEqual(self.ephemera.speed, 5)
 
     def test_add_numina(self):
-        self.fail()
+        numina = Numina.objects.create()
+        self.ephemera.add_numina(numina)
+        self.assertEqual(self.ephemera.numina.count(), 1)
+        self.assertIn(numina, self.ephemera.numina.all())
 
-    def test_add_filter_numina(self):
-        self.fail()
+    def test_filter_numina(self):
+        numina1 = Numina.objects.create()
+        numina2 = Numina.objects.create()
+        num = self.ephemera.filter_numina().count()
+        self.ephemera.add_numina(numina1)
+        self.assertEqual(self.ephemera.filter_numina().count(), num - 1)
 
     def test_total_numina(self):
-        self.fail()
+        numina1 = Numina.objects.create()
+        numina2 = Numina.objects.create()
+        self.ephemera.add_numina(numina1)
+        self.assertEqual(self.ephemera.total_numina(), 1)
+        self.ephemera.add_numina(numina2)
+        self.assertEqual(self.ephemera.total_numina(), 2)
 
 
 class TestRandomEphemera(TestCase):
+    def setUp(self):
+        self.ephemera = Ephemera.objects.create()
+
     def test_random_rank(self):
-        self.fail()
+        ranks = [1, 2, 3, 4, 5]
+        self.ephemera.random_rank()
+        self.assertIn(self.ephemera.rank, ranks)
 
     def test_random_type(self):
-        self.fail()
+        types = ["spirit", "goetia", "ghost", "supernal being"]
+        self.ephemera.random_type()
+        self.assertIn(self.ephemera.ephemera_type, types)
 
     def test_random_attributes(self):
-        self.fail()
+        self.ephemera.rank = 2
+        self.ephemera.random_attributes()
+        total_dots = self.ephemera.total_attributes()
+        rank_to_dots = {
+            1: list(range(5, 8 + 1)),
+            2: list(range(9, 14 + 1)),
+            3: list(range(15, 25 + 1)),
+            4: list(range(26, 35 + 1)),
+            5: list(range(36, 45 + 1)),
+        }
+        rank = self.ephemera.rank
+        self.assertGreaterEqual(total_dots, rank_to_dots[rank][0])
+        self.assertLessEqual(total_dots, rank_to_dots[rank][-1])
 
     def test_random_numina(self):
-        self.fail()
+        numina = Numina.objects.create(name="Test Numina")
+        self.ephemera.random_numina()
+        self.assertGreaterEqual(self.ephemera.total_numina(), 0)
 
     def test_random_name(self):
-        self.fail()
+        name = "Test Ephemera"
+        self.ephemera.random_name(name)
+        self.assertEqual(self.ephemera.name, name)
 
     def test_random(self):
-        self.fail()
+        ranks = [1, 2, 3, 4, 5]
+        types = ["spirit", "goetia", "ghost", "supernal being"]
+        for i in range(10):
+            Numina.objects.create(name=f"Test Numina {i}")
+        self.ephemera.random()
+        self.assertIn(self.ephemera.rank, ranks)
+        self.assertIn(self.ephemera.ephemera_type, types)
+        self.assertGreaterEqual(self.ephemera.total_attributes(), self.ephemera.rank)
+        self.assertGreaterEqual(self.ephemera.total_numina(), 0)
